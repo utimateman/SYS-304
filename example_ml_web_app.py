d99bb9f0ca159490a0235b7c42dbd078cbc9148f
utimateman <krai020913@gmail.com>
2023-03-08 21:01:44 +0700

    finish website

--- example_ml_web_app.py
+++ example_ml_web_app.py
@@ -1,12 +1,44 @@
-import pickle
+# import pickle
+import streamlit as st
+from PIL import Image
 
 
-# Load the ML model
-filename = 'shoe_size_prediction_model.pkl'
-with open(filename, 'rb') as file:
-    loaded_model = pickle.load(file)
+def predictShoeSize(height):
+    predicted_shoe_size = 999
+    # Load the ML model
+    # filename = 'shoe.size.prediction.model.pkl'
+    # with open(filename, 'rb') as file:
+    #     loaded.model = pickle.load(file)
 
-# Use the loaded model to make predictions
-new_X = [[165], [170], [175]]
-predictions = loaded_model.predict(new_X)
-print(predictions)
+    # # Use the loaded model to make predictions
+    # new.X = [[height]]
+    # predicted_shoe_size = loaded.model.predict(new.X)
+    # print(predicted_shoe_size)
+
+    return predicted_shoe_size
+
+st.markdown("***")
+st.markdown("# Let's Predict your Shoe Size from your Height")
+st.markdown("***")
+
+height_row, space, shoe_size_row = st.columns([2,1,2])
+
+with height_row:
+    st.image("https://drive.google.com/uc?id=17A15LbY0ldA3uP3tDG-mAOBtZAeW6OJ7", use_column_width=True) 
+
+    
+
+with shoe_size_row:
+    height = st.number_input('Insert your height (cm):')
+    predicted_shoe_size = predictShoeSize(height)
+    st.markdown("  ")
+    st.markdown("  ")
+    st.markdown("Your shoe size is:")
+    st.markdown("# " + str(predicted_shoe_size))
+    st.markdown("  ")
+    st.markdown("  ")
+
+
+
+
+st.markdown("***")
